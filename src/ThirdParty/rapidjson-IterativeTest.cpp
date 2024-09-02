30d620e9200294cac4a671d76293bf92bea9d1ea
Loki Astari <Loki.Astari@gmail.com>
2024-09-02 09:48:01 -0700

    Remove Tests not needed for Glaze

--- src/ThirdParty/rapidjson-IterativeTest.cpp
+++
@@ -1,9 +0,0 @@
-#define TEST_ITERATIVE
-#define TEST_PARSE_FLAG kParseIterativeFlag
-#define TEST_NAME "rapidjsonIterative"
-#define TEST_CLASS RapidjsonIterativeTest
-#define RapidjsonParseResult RapidjsonParseIterativeResult
-
-#include "rapidjsonTest.h"
-
-REGISTER_TEST(RapidjsonIterativeTest);
