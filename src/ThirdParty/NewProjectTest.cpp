21b27c93d8ac015ec5a9c9ee414b6f8d357d85a8
Loki Astari <Loki.Astari@gmail.com>
2024-09-02 15:02:35 -0700

    Clarify comments

--- src/ThirdParty/NewProjectTest.cpp
+++ src/ThirdParty/NewProjectTest.cpp
@@ -17,7 +17,7 @@ public:
     {
         // This function is optional.
         // If you don't provide it then it uses Parse().
-        // If Parse() retruns a value in its `reply` then it will set the above reply to true otherwise false.
+        // If Parse() returns a value in its `reply` then it will set the above reply to true otherwise false.
 
 
         // Has to return true to indicate that it implemented the function.
@@ -31,15 +31,15 @@ public:
         return true;
     }
 #endif
-    virtual bool Parse(const char* j, size_t length, std::unique_ptr<ParseResultBase>& reply) const override
+    virtual bool Parse(const char* json, size_t length, std::unique_ptr<ParseResultBase>& reply) const override
     {
-        // Parse the json string in "j" with length "length"
+        // Parse the JSON string in "json" with length "length"
 
         // Your intermediate result is stored in "reply".
-        // If you fail to parse (or there is an error in the json) then don't set reply (just leave it null).
+        // If you fail to parse (or there is an error in the JSON) then don't set reply (just leave it null).
 
         // Note every JSON parser is different so you should define your own reply structure.
-        // See the "UnivalueResult" above, configure this to hold your intermediate data.
+        // See the "NewProjectResult" above, configure this to hold your intermediate data.
 
         // The value returned in "reply" will be passed as the first value to Stringify() and Prettify() (if it is not null).
         return true;
@@ -66,7 +66,7 @@ public:
 
     virtual bool ParseDouble(const char* j, long double& d) const override
     {
-        // The input json is an array with a single double value.
+        // The input JSON is an array with a single double value.
         // Read the double from the array and assign to d.
         //
         // Note: There are lots of valid double formats you are expected to be able to decode them.
@@ -76,11 +76,11 @@ public:
 
     virtual bool ParseString(const char* j, std::string& s) const override
     {
-        // The input json is an array with a single string value.
+        // The input JSON is an array with a single string value.
         // Read the string from the array and assign to s.
         //
-        // Note: There are lots of escape sequence in JSON you are expected to dcode these into the C++ representation of the string.
-        //       EG: the '\t' sequence is expected to be decoded into the "Tab" character. etc.
+        // Note: There are lots of escape sequence in JSON you are expected to decode these into the C++ representation of the string.
+        //       e.g.: the '\t' sequence is expected to be decoded into the "Tab" character. etc.
         return true;
     }
 };
