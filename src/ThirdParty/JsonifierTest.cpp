429f4e47d0908881ae75b29cd61396b1fce46140
Loki Astari <Loki.Astari@gmail.com>
2024-09-06 11:59:00 -0700

    Add Jsonifier parser

---
+++ src/ThirdParty/JsonifierTest.cpp
@@ -0,0 +1,46 @@
+
+#include "test.h"
+#include "TypeSafe.h"
+#include "JsonifierCatalog.h"
+#include "JsonifierCountry.h"
+#include "JsonifierTwitter.h"
+#include "JsonifierTypes.h"
+
+
+// OK: The TypeSafeTest test is a template type that has a handler for each test case.
+//     There are three default handlers that you need to provide.
+//          class VectorDouble: public TestAction
+//          class VectorString: public TestAction
+//          template<typename T>
+//          class GetValue: public TestAction
+//
+// See the file: JsonifierTypes.h for the framework.
+//
+// This should cover most cases.
+// If you need to do some special handling of types you can override the default actions.
+// See comments blow:
+
+class JsonifierTest: public TypeSafeTest<JsonifierTypes::VectorDouble,      // Type for parsing array of single double.
+                                               JsonifierTypes::VectorString,      // Type ofr parsing array of single string
+                                               JsonifierTypes::GetValue>          // Template template class. This is templatezed with the type
+                                                                                        // That needs to be read from the json string.
+{
+    JsonifierTypes::GetValue<std::vector<std::string>>          testGetValueVec2StringNumber;
+    JsonifierTypes::GetValue<canada_message>                                             testGetValueCountry;
+    JsonifierTypes::GetValue<twitter_message>                                             testGetValueTwitter;
+    JsonifierTypes::GetValue<citm_catalog_message>                                             testGetValueCatalog;
+  public:
+      JsonifierTest()
+      {
+          actionMap["performance/canada.json"] = &testGetValueCountry;
+          actionMap["performance/twitter.json"] = &testGetValueTwitter;
+          actionMap["performance/citm_catalog.json"] = &testGetValueCatalog;
+      }
+
+        virtual const char* GetName()     const override    { return "Jsonifier"; }
+        virtual const char* Type()        const override    { return "C++";}
+        virtual const char* GetFilename() const override    { return __FILE__; }
+};
+
+REGISTER_TEST(JsonifierTest);
+
