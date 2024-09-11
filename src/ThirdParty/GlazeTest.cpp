f7fb231fe72bf12cfc8664ede0e679143791da12
Loki Astari <Loki.Astari@gmail.com>
2024-09-11 11:36:04 -0700

    Glaze now compiles with the adjusted types

--- src/ThirdParty/GlazeTest.cpp
+++ src/ThirdParty/GlazeTest.cpp
@@ -89,17 +89,15 @@ class GlazeTest: public TypeSafeTest<Glaze::VectorDouble,
                                      Glaze::VectorString,
                                      Glaze::GetValue>
 {
-    // Not supported by glaze as these types are to complicated.
-    // I would love help if you can get these to compile.
     Glaze::GetValue<Perform>                                             testGetValueCatalog;
-    //Glaze::GetValue<Country>                                             testGetValueCountry;
-    //Glaze::GetValue<Twitter>                                             testGetValueTwitter;
+    Glaze::GetValue<Country>                                             testGetValueCountry;
+    Glaze::GetValue<Twitter>                                             testGetValueTwitter;
     public:
         GlazeTest()
         {
             actionMap["performance/citm_catalog.json"]   = &testGetValueCatalog;
-            //actionMap["performance/canada.json"]         = &testGetValueCountry;
-            //actionMap["performance/twitter.json"]        = &testGetValueTwitter;
+            actionMap["performance/canada.json"]         = &testGetValueCountry;
+            actionMap["performance/twitter.json"]        = &testGetValueTwitter;
         }
 
         virtual const char* GetName()     const override    { return "Glaze"; }
